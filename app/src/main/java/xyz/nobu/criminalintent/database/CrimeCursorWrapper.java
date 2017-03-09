e0c8f6e3b753d3ccebf5e6ff5fde9041ce6c8cc7
Nobuyoshi Aquino <nobuyoshi.aquino@gmail.com>
2017-03-08 23:45:26 -0300

    Replace the volatile crime arraylist for a SQLite database implementation to store crimes

---
+++ app/src/main/java/xyz/nobu/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,35 @@
+package xyz.nobu.criminalintent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import java.util.Date;
+import java.util.UUID;
+
+import xyz.nobu.criminalintent.Crime;
+
+import static xyz.nobu.criminalintent.database.CrimeDbSchema.*;
+
+/**
+ * Created by nobu on 3/8/17.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+
+        return crime;
+    }
+}
